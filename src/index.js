332cf3e20390826b6fe181facf1afd0f07f61019
Vishal Heble <visheble@yahoo.com>
2018-03-29 01:22:33 +0530

    Added acions type

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'react';
+import chat from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(chat);
+
+ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      document.getElementById('root')
+    );
 registerServiceWorker();
